c954b9a7c0fd1b0afeae7225771d4d0ccd60b026
Eugene <evgenii.is.the.best.geek@gmail.com>
2019-01-10 14:46:47 +0200

    Implementing the Button Component

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,7 +2,13 @@ import React from 'react'
 import classes from './Backdrop.module.sass'
 
 const backdrop = props => (
-    props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
+    props.show ? <div 
+        className={classes.Backdrop} 
+        onClick={props.clicked}
+        style={{
+            opacity: props.show ? (setTimeout(()=>'1',)) : '0'
+        }}   
+    ></div> : null
 )
 
 export default backdrop

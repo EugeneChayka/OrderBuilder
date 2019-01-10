8907c5996aa58017a891b0b18300abfc844a5008
Eugene <evgenii.is.the.best.geek@gmail.com>
2019-01-10 15:08:29 +0200

    Adding a Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Aux from '../../HOC/Aux'
 import classes from './Layout.module.sass'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = props => (
     <Aux>
+        <Toolbar/>
         <div>Toolbar, SideDraver, Backdrop</div>
         <main className={classes.Content}>
             {props.children}

88f7ba29ada8f34757d0e0c3b3116cbd74c35b2d
Eugene <evgenii.is.the.best.geek@gmail.com>
2019-01-25 00:36:04 +0200

    Displaying User Specific Orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -53,10 +53,11 @@ export const fetchOrderStart = () => {
     }
 }
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => { // other way - can add getState and get token from state
         dispatch(fetchOrderStart())
-        axios.get('orders.json?auth=' + token)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"'
+        axios.get('orders.json' + queryParams)
             .then(res => {
                 const fetchedOrders = []
                 for (let key in res.data) {

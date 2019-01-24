e39e075d08533becb19f279d17c160d36a46e265
Eugene <evgenii.is.the.best.geek@gmail.com>
2019-01-24 15:18:35 +0200

    Logging Users Out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,22 +22,40 @@ export const authFail = error => {
     }
 }
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = expirationTime => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout())
+        }, expirationTime * 1000)
+    }
+}
+
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart())
+
         const authData = {
             email,
             password,
             returnSecureToken: true
         }
+
         let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDoBDLxOGkLbfPgXqxUuYd0Y2C6AMOMXOg'
         if (!isSignup) {
             url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDoBDLxOGkLbfPgXqxUuYd0Y2C6AMOMXOg'
         }
+
         axios.post(url, authData)
             .then(response => {
                 console.log(response)
                 dispatch(authSuccess(response.data.idToken, response.data.localId))
+                dispatch(checkAuthTimeout(response.data.expiresIn))
             })
             .catch(err => {
                 console.log(err)

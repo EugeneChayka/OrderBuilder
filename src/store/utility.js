aee00ebe413dfd1420a5657417e204f168a9ce0a
Eugene <evgenii.is.the.best.geek@gmail.com>
2019-01-24 08:40:43 +0200

    Refactoring Reducers Continued

--- src/store/utility.js
+++ src/store/utility.js
@@ -1,6 +1,6 @@
-export const updateObject = (oldObject, updaterProperties) => {
+export const updateObject = (oldObject, updatedProperties) => {
     return {
         ...oldObject,
-        ...updaterProperties
+        ...updatedProperties
     }
 }
